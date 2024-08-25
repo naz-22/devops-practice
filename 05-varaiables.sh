83fe4a1fb7dedfc14d22f58b454b8102cf515876
patan naziya <patannaziyatasleem@gmail.com>
2024-08-25 12:33:25 +0530

    added third shell script

--- 05-varaiables.sh
+++ 05-varaiables.sh
@@ -1,7 +1,7 @@
 #/!/bin/bash
 echo "please enter your username::"
 
-read -s USERNAME # takes input into USERNAME variable
+read -s USERNAME # read statment takes input into USERNAME variable and -s will hide your details
 
 echo "username entered is::$USERNAME"
 
