b8a893d117042c4c68e43e1b346a474a75653765
patan naziya <patannaziyatasleem@gmail.com>
2024-08-25 11:40:53 +0530

    added coversation shell script

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,5 +2,5 @@
 echo "Ramesh::Hi suresh.how are you?"
 echo "Suresh::Hi ramesh.I am fn tq, how  are you?"
 echo "Ramesh::I am doing good suresh, how about you?"
-echo "Suresh::I started learning Shell Script Ramesh?"
+echo "Suresh::I started learning Shell Script Ramesh"
 
