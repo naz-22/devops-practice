c0df3a69743836a17de84f8108460f676a3a1985
patan naziya <patannaziyatasleem@gmail.com>
2024-08-25 12:06:32 +0530

    added third shell script

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1::Hi $PERSON2.how are you?"
+echo "$PERSON2::Hi $PERSON1.I am fn tq, how  are you?"
+echo "$PERSON1::I am doing good $PERSON2, how about you?"
+echo "$PERSON2::I started learning Shell Script $PERSON1"
