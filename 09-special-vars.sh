e73cc924c3b1bf7e92cad79939e8f101ef028f58
patan naziya <patannaziyatasleem@gmail.com>
2024-08-26 22:57:25 +0530

    added third shell script

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -1,4 +1,11 @@
 #/bin/bash
+#--------- speacial variables & their descprition -----#
 
-echo "all variables passed to the script: $@"
-echo "Number of  variables passed : $#"
+#--> $@ -->reads the all variables passed to the script.
+echo "all variables passed to the script: $@"  
+
+#--> $# -->reads the count of variables passed to the script.
+echo "Number of  variables passed : $#" 
+
+#--> $0 -->reads the name of  your  script.
+echo "script name : $0"
