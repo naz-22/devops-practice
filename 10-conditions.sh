e3886658ed740582b0a34710f0614961cfea0a95
patan naziya <patannaziyatasleem@gmail.com>
2024-08-26 23:56:16 +0530

    added third shell script

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -8,5 +8,5 @@ if [ $NUMBER -gt 20 ] # gt ,lt, eq , -ne , -ge , -le
 then 
     echo "given number : $NUMBER is greater than 20"
 else
-     echo "given number : $NUMBER is grless than 20"
+     echo "given number : $NUMBER is less than 20"
 fi
