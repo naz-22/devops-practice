c4acfa8e7d8068c56968e5f3e72d07d77f00eefc
patan naziya <patannaziyatasleem@gmail.com>
2024-08-31 12:07:13 +0530

    added 11th shell script

--- 11-installation.sh
+++ 11-installation.sh
@@ -7,11 +7,18 @@ then
     exit 1
 fi
 
-dnf list installed git 
+dnf list installed gitttt
 if [ $? -ne 0 ]
 then 
     echo "git not installed, going to install it .."
-    dnf install git -y
+    dnf install gittt -y
+    if [ $? -ne 0 ]
+    then 
+        echo "git installation is not success..., check it"
+        exit 1
+    else
+        echo "git installation is success..."
+    fi    
 else
     echo "git is already installed, nothing to do.."
 
