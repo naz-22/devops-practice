eb2197957e3d6758dfb3a12ab4fa80a8f95fbdee
patan naziya <patannaziyatasleem@gmail.com>
2024-08-31 15:31:49 +0530

    added 11th shell script

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-echo "1"
-echo "2"
-echo "3"
-echo "4"
-echo "5"
-echo "6"
-echo "7"
-echo "8"
-echo "9"
-echo "10"
+for i in {0..10} #it means 0 to 10 values
+do
+    echo $i
+done    
+# echo "1"
+# echo "2"
+# echo "3"
+# echo "4"
+# echo "5"
+# echo "6"
+# echo "7"
+# echo "8"
+# echo "9"
+# echo "10"
