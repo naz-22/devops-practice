20830e9474e3b2b02a37e7a1e96b7f01dd3078ad
patan naziya <patannaziyatasleem@gmail.com>
2024-08-31 11:16:11 +0530

    added 11th shell script

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,3 +1,12 @@
 #!/bin/bash/
 USERID=$(id -u)
-echo "USER ID IS : $USERID"
+#echo "USER ID IS : $USERID"
+if [ $USERID -ne 0  ]
+then
+    echo "plese run the script with root priviliges"
+else
+
+fi
+
+dnf install git -y
+
