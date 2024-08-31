da69685665a26abf55fa53479e28b7d3416dd544
patan naziya <patannaziyatasleem@gmail.com>
2024-08-31 12:51:44 +0530

    added 11th shell script

---
+++ 12-functions.sh
@@ -0,0 +1,48 @@
+#!/bin/bash/
+USERID=$(id -u)
+#echo "USER ID IS : $USERID"
+VALIDATE()
+{
+  echo "exit status : $1"
+}
+if [ $USERID -ne 0  ]
+then
+    echo "plese run the script with root priviliges"
+    exit 1
+fi
+
+dnf list installed git
+
+VALIDATE $?
+
+# if [ $? -ne 0 ]
+# then 
+#     echo "git not installed, going to install it .."
+#     dnf install git-y
+#     if [ $? -ne 0 ]
+#     then 
+#         echo "git installation is not success..., check it"
+#         exit 1
+#     else
+#         echo "git installation is success..."
+#     fi    
+# else
+#     echo "git is already installed, nothing to do.."
+
+# fi    
+
+# dnf list installed mysql
+# if [ $? -ne 0 ]
+# then 
+#     echo " mysql is not installed.., going to install it.."
+#     dnf install mysql -y
+#     if [ $? -ne 0]
+#     then 
+#         echo "mysql installation is failure..pease check it.."
+#         exit 1
+#     else  
+#         echo "mqsql installation is success.."
+#     fi
+# else
+#     echo "mqsql is already installed .. nothing to do."
+# fi   
