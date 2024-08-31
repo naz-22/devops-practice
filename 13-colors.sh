2d3d955643d603c76b0f19f0fc86d392e5e8478d
patan naziya <patannaziyatasleem@gmail.com>
2024-08-31 13:58:08 +0530

    added 11th shell script

---
+++ 13-colors.sh
@@ -0,0 +1,46 @@
+#!/bin/bash/
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+G="\e[0m"
+CHECK_ROOT()
+{
+if [ $USERID -ne 0  ]
+then
+    echo "plese run the script with root priviliges"
+    exit 1
+fi
+}
+
+VALIDATE()
+{
+if [ $1 -ne 0 ]
+then 
+    echo -e "$2 is $R failed  $N"
+    exit 1
+else 
+    echo -e "$2 is  $G success $N"
+fi 
+}
+
+CHECK_ROOT
+dnf list installed git
+
+if [ $? -ne 0 ]
+then 
+    echo "git not installed, going to install it .."
+    dnf install git-y
+    VALIDATE $? "installing git"
+else
+    echo "git is already installed, nothing to do.."
+fi    
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+    echo " mysql is not installed.., going to install it.."
+    dnf install mysql -y
+    VALIDATE $? "installing mysql"
+else
+    echo "mqsql is already installed .. nothing to do."
+fi   
