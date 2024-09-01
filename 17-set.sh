2ea021a853468bd8efff4901a023b67e1d1ce033
patan naziya <patannaziyatasleem@gmail.com>
2024-09-01 16:22:48 +0530

    added frontend shell script

--- 17-set.sh
+++ 17-set.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # this is kind of shell script to automate the VALIDATE FUNCTION.
-set -ex # it means setting the automatic exit, if we get error here x means debug./-ex means exit with debugging.
+set -e # it means setting the automatic exit, if we get error. we can use  set-ex   for debugging.while analyzing the logs in detail.
+failure(){
+echo "Failed at : $1:$2"
+
+}
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR # ERR is the error signal
 echo "hello world success."
 echhooooo "hello world failure"
 echo "hello world after failure"
