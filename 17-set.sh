732d0118ecec1823c9b23ee94b7ba50cf5d50127
patan naziya <patannaziyatasleem@gmail.com>
2024-09-01 16:10:22 +0530

    added frontend shell script

--- 17-set.sh
+++ 17-set.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # this is kind of shell script to automate the VALIDATE FUNCTION.
-set -e # it means setting the automatic exit, if we get error
+set -ex # it means setting the automatic exit, if we get error here x means debug./-ex means exit with debugging.
 echo "hello world success."
 echhooooo "hello world failure"
 echo "hello world after failure"
